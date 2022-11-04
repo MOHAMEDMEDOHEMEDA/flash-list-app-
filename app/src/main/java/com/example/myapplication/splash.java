448aecd44f67a1a4492b615c1519c2dca3a7bf18
mahmoud essam <mahmoudessam0123@gmail.com>
2022-11-04 21:52:27 +0200

    I have put splash style code and made the background using Figma, With handling it with 3 secs Put text in Main Activity and changed the name and logo of the app 4/11/2022 9:52 pm

---
+++ app/src/main/java/com/example/myapplication/splash.java
@@ -0,0 +1,25 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        getSupportActionBar().hide();
+        Handler x = new Handler();
+        x.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(getApplicationContext(),MainActivity.class));
+                finish();
+            }
+        },3000);
+    }
+}
