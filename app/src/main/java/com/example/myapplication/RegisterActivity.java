df3502ac65dd2512dd071a53b0fdde36b7cea3ed
mahmoud essam <mahmoudessam0123@gmail.com>
2022-11-06 00:42:24 +0200

    Now FireBase Works !! Register and Login works with fire base and the app will work soon and you can use it 12:42 am 6 nov 2022

--- app/src/main/java/com/example/myapplication/RegisterActivity.java
+++ app/src/main/java/com/example/myapplication/RegisterActivity.java
@@ -23,7 +23,6 @@ public class RegisterActivity extends AppCompatActivity {
     private EditText email, password;
     private Button btnRegister;
     private TextView textLogin;
-    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
